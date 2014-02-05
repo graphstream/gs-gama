9f61d46988d7af3756d2fc537ac48ca7105bd8b7
Thibaut Démare <fdhp_76@hotmail.com>
2014-02-05 18:31:27 +0100

    Add the 'step' command

---
+++ src/org/graphstream/gama/extension/sender/StepStatement.java
@@ -0,0 +1,45 @@
+package org.graphstream.gama.extension.sender;
+
+import msi.gama.precompiler.ISymbolKind;
+import msi.gama.precompiler.GamlAnnotations.facet;
+import msi.gama.precompiler.GamlAnnotations.facets;
+import msi.gama.precompiler.GamlAnnotations.inside;
+import msi.gama.precompiler.GamlAnnotations.symbol;
+import msi.gama.runtime.IScope;
+import msi.gama.runtime.exceptions.GamaRuntimeException;
+import msi.gaml.descriptions.IDescription;
+import msi.gaml.expressions.IExpression;
+import msi.gaml.statements.AbstractStatement;
+import msi.gaml.statements.IStatement;
+import msi.gaml.types.IType;
+
+import org.graphstream.gama.extension.GSManager;
+import org.graphstream.gama.extension.IKeywordGSAdditional;
+
+
+@symbol(name = IKeywordGSAdditional.STEP, kind = ISymbolKind.SINGLE_STATEMENT, with_sequence = false)
+@inside(kinds = { ISymbolKind.BEHAVIOR, ISymbolKind.SINGLE_STATEMENT })
+@facets(value = { @facet(name = IKeywordGSAdditional.SENDERID, type = IType.STRING, optional = false),
+		 @facet(name = IKeywordGSAdditional.STEP_NUMBER, type = IType.INT, optional = false)})
+public class StepStatement extends AbstractStatement implements IStatement{
+	
+	final IExpression senderid;
+	final IExpression step_number;
+	
+	public StepStatement(IDescription desc) {
+		super(desc);
+		senderid = getFacet(IKeywordGSAdditional.SENDERID);
+		step_number = getFacet(IKeywordGSAdditional.STEP_NUMBER);
+	}
+
+	@Override
+	protected Object privateExecuteIn(IScope scope) throws GamaRuntimeException {
+		String senderId = (String)(senderid.value(scope));
+		int step =(Integer)(step_number.value(scope));
+		GSSender sender = GSManager.getSender(senderId);
+		sender.sendStepBegins(step);
+		return null;
+	}
+
+	
+}
