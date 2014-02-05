123c7aa57f57d2ea2f3ef7424328cbcc77111af5
Thibaut Démare <fdhp_76@hotmail.com>
2014-02-05 17:20:14 +0100

    Add a file describing the new keywords of the gs plugin

---
+++ src/org/graphstream/gama/extension/IKeywordGSAdditional.java
@@ -0,0 +1,21 @@
+package org.graphstream.gama.extension;
+
+import msi.gama.common.interfaces.IKeyword;
+
+public class IKeywordGSAdditional implements IKeyword {
+	// New commands
+	
+	public static final String ADD = "gs_add";
+	public static final String ADD_ATTRIBUTE = "gs_add_attribute";
+	public static final String ADD_SENDER = "gs_add_sender";
+	public static final String CLEAR = "gs_clear";
+	public static final String CLEAR_SENDER = "gs_clear_sender";
+	public static final String REMOVE = "gs_remove";
+	public static final String REMOVE_ATTRIBUTE = "gs_remove_attribute";
+	public static final String STEP = "gs_step";
+	
+	
+	// Facets associated to a command
+	
+	public static final String SENDERID = "gs_sender_id";
+}
